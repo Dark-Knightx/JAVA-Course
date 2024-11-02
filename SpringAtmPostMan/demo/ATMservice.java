13777a279a63de168050017aa35641839ddb49c3
Dark-Knightx <darkxknightx69@gmail.com>
2024-11-02 23:58:35 +0530

    Update ATMservice.java

--- SpringAtmPostMan/demo/ATMservice.java
+++ SpringAtmPostMan/demo/ATMservice.java
@@ -15,14 +15,30 @@ public class ATMservice {
 	
 	public void deposit(int accountNo, int amount) {
 		ATMmodal user = atmRepo.findByAccountNo(accountNo); // You'll need to create this method in the repository
-        if (user != null) {
-            // Update the amount
-            user.setAmount(user.getAmount() + amount);
-            atmRepo.save(user); // Save the updated user back to the database
-        } else {
-            // Handle the case where the user is not found (optional)
-            System.out.println("User with account number " + accountNo + " not found.");
-        }
+	        if (user != null) {
+	            // Update the amount
+	            user.setAmount(user.getAmount() + amount);
+	            atmRepo.save(user); 
+	        } else {
+	            System.out.println("User with account number " + accountNo + " not found.");
+	        }
 	}
-	
+
+	public void withdraw(int accountNo,int pin, int amount) {
+		ATMmodal user = atmRepo.findByAccountNo(accountNo); // You'll need to create this method in the repository
+	        if (user != null) {
+	        	if (user.getPin()==pin) {
+	        		if (user.getAmount()>=amount) {
+	        			user.setAmount(user.getAmount() - amount);
+	                    atmRepo.save(user); 
+				}else {
+						System.out.println("Insufficient Balance");
+				}
+			}else {
+				System.out.println("WrongPin");
+			}
+	        } else {
+	            System.out.println("User with account number " + accountNo + " not found.");
+	        }
+	}
 }
