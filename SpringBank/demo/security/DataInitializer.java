f7fe69cbba140f8f827f82f1dee40c8ab7a28bff
Dark-Knightx <darkxknightx69@gmail.com>
2024-11-17 01:02:10 +0530

    Update DataInitializer.java

--- SpringBank/demo/security/DataInitializer.java
+++ SpringBank/demo/security/DataInitializer.java
@@ -1,5 +1,6 @@
-// package com.example.demo.Security;
+package com.example.demo.security;
 
+import com.example.demo.repository.UserRepository;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -11,18 +12,18 @@ public class DataInitializer {
     @Bean
     public CommandLineRunner initializeUsers(UserRepository userRepository, PasswordEncoder passwordEncoder) {
         return args -> {
-            if (userRepository.findByUsername("admin") == null) {
-                com.example.demo.Security.User admin = new com.example.demo.Security.User();
-                admin.setUsername("admin");
-                admin.setPassword(passwordEncoder.encode("admin123")); // Encrypt password
+            if (userRepository.findByUsername("Valkyrae") == null) {
+                com.example.demo.modal.User admin = new com.example.demo.modal.User();
+                admin.setUsername("Valkyrae");
+                admin.setPassword(passwordEncoder.encode("asdfress")); // Encrypt password
                 admin.setRole("ADMIN");
                 userRepository.save(admin);
             }
 
-            if (userRepository.findByUsername("user") == null) {
-                com.example.demo.Security.User user = new com.example.demo.Security.User();
-                user.setUsername("user");
-                user.setPassword(passwordEncoder.encode("user123")); // Encrypt password
+            if (userRepository.findByUsername("Leslie") == null) {
+                com.example.demo.modal.User user = new com.example.demo.modal.User();
+                user.setUsername("Leslie");
+                user.setPassword(passwordEncoder.encode("asdfrezz")); // Encrypt password
                 user.setRole("USER");
                 userRepository.save(user);
             }
