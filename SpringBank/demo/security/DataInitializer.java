f33b78ecc800894867940be80d596fd747a72a09
Dark-Knightx <darkxknightx69@gmail.com>
2024-11-23 19:00:06 +0530

    Update DataInitializer.java

--- SpringBank/demo/security/DataInitializer.java
+++ SpringBank/demo/security/DataInitializer.java
@@ -1,6 +1,7 @@
 package com.example.demo.security;
 
 import com.example.demo.repository.UserRepository;
+import com.example.demo.repository.AdminRepo;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -10,22 +11,22 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 public class DataInitializer {
 
     @Bean
-    public CommandLineRunner initializeUsers(UserRepository userRepository, PasswordEncoder passwordEncoder) {
+    public CommandLineRunner initializeUsers(UserRepository userRepository, AdminRepo adminRepo, PasswordEncoder passwordEncoder) {
         return args -> {
             if (userRepository.findByUsername("Valkyrae") == null) {
-                com.example.demo.modal.User admin = new com.example.demo.modal.User();
+                com.example.demo.modal.User user = new com.example.demo.modal.User();
                 admin.setUsername("Valkyrae");
                 admin.setPassword(passwordEncoder.encode("asdfress")); // Encrypt password
-                admin.setRole("ADMIN");
+                admin.setRole("USER");
                 userRepository.save(admin);
             }
 
-            if (userRepository.findByUsername("Leslie") == null) {
-                com.example.demo.modal.User user = new com.example.demo.modal.User();
-                user.setUsername("Leslie");
-                user.setPassword(passwordEncoder.encode("asdfrezz")); // Encrypt password
-                user.setRole("USER");
-                userRepository.save(user);
+            if (adminRepo.findByUsername("Pokimane") == null) {
+                Admin admin = new com.example.demo.modal.Admin();
+                admin.setUsername("Pokimane");
+                admin.setPassword(passwordEncoder.encode("asdfaaaa")); // Encrypt password
+                admin.setRole("ADMIN");
+                adminRepo.save(admin);
             }
         };
     }
